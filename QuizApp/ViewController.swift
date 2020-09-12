baee47a5a1b96982a7c25783fa1d67b0d9d50f65
Osvaldo Garcia <osgar@icloud.com>
2020-09-12 06:22:09 -0400

    Display Questions and answers.  User selection of answer and check the answer

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
     var model = QuizModel()
     var questions = [Question]()
     var currentQuestionIndex = 0
+    var numCorrect = 0
     
 
     override func viewDidLoad() {
@@ -34,15 +35,29 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
         model.getQuestions()
     }
     
+    func displayQuestion() {
+        
+        //Check if there are questions and check that the currentQuestionIndex is not out of bound
+        guard questions.count > 0 && currentQuestionIndex < questions.count else{
+            return
+        }
+        
+        //Display question text
+        questionLabel.text = questions[currentQuestionIndex].question
+        
+        //Reload the answer label
+        tableView.reloadData()
+        
+    }
+    
     // MARK: - QuizProtocol Methods
     func questionRetreive(_ questions: [Question]) {
         
         //Get a reference to the questions
         self.questions = questions
         
-        //Reload the table view
-        tableView.reloadData()
-        
+        //Display the first question
+        displayQuestion()
     }
     
     //MARK: -UITableview delegate methods
@@ -74,8 +89,13 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
         let label = cell.viewWithTag(1) as? UILabel
         
         if label != nil{
+            let question = questions[currentQuestionIndex]
+            
+            if question.answers != nil && indexPath.row < question.answers!.count {
         
-        //TODO: Set the answer text for the label
+                //Set the answer text for the label
+                label!.text = question.answers![indexPath.row]
+            }
             
         }
         
@@ -83,7 +103,30 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
         //Return the cell
         return cell
     }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //User have tapped on a row, check if its the right answer
+        let question = questions[currentQuestionIndex]
+        
+        if question.correctAnswerIndex! == indexPath.row {
+            
+            //User got it right
+            print("User got it right")
+        }
+        else {
+            
+            //User got it wrong
+            print("User got it wrong")
+        }
+        
+        //Increment currentQuestionIndex
+        currentQuestionIndex += 1
+        
+        //Display the next question
+        displayQuestion()
+        
+        
+    }
     
 
 
